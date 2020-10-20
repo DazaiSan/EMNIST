d671b52ca92b6c0172a637c53fd24a1b6006536b
DazaiSan <amim@cinnamon.is>
2020-10-20 13:58:24 +0900

    prerequisite packages added

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 from setuptools import find_packages
 
+REQUIRED_PACKAGES = ['torch>=1.5', 'scikit-learn>=0.20', 'pandas>=1.0']
+
 setup(
 	name='emnist-train',
 	version='1.0.0',
+    install_requires=REQUIRED_PACKAGES,
 	packages=find_packages(),
 	include_package_data=True,
 	description='ML Model training for Emnist data with PyTorch frameowrk'
